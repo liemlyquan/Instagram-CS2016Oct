36d9e1771728dbc6ecf2a9646e9f542fb5abc9d6
Liem Ly Quan <liemlyquan@gmail.com>
2016-10-12 19:16:13 +0700

    Initial Commit

---
+++ Instagram-CS2016Oct/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram-CS2016Oct
+//
+//  Created by Liem Ly Quan on 10/12/16.
+//  Copyright © 2016 coderschool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
