6eb44302d4cf3d6899e73de29fe01196d6365b7c
Nguyen the phuong <nguyenphuong.it242@gmail.com>
2016-10-12 20:09:11 +0700

    finish 5th milstone

--- Instagram-CS2016Oct/PhotosViewController.swift
+++ Instagram-CS2016Oct/PhotosViewController.swift
@@ -22,6 +22,11 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
         loadPhotos()
         tableView.dataSource = self
         tableView.delegate = self
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
+
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -61,6 +66,39 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
         }
     }
     
+    func refreshControlAction(_ refreshControl: UIRefreshControl) {
+        let accessToken = "435569061.c66ada7.d12d19c8687e427591f254586e4f3e47"
+        let userId = "435569061"
+        let url = URL(string: "https://api.instagram.com/v1/users/\(userId)/media/recent/?access_token=\(accessToken)")
+        
+        if let url = url {
+            let request = URLRequest(
+                url: url,
+                cachePolicy: NSURLRequest.CachePolicy.reloadIgnoringLocalCacheData,
+                timeoutInterval: 10)
+            let session = URLSession(
+                configuration: URLSessionConfiguration.default,
+                delegate: nil,
+                delegateQueue: OperationQueue.main)
+            let task = session.dataTask(
+                with: request,
+                completionHandler: { (dataOrNil, response, error) in
+                    if let data = dataOrNil {
+                        if let responseDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                            print("response: \(responseDictionary)")
+                            if let photoData = responseDictionary["data"] as? [NSDictionary] {
+                                self.photos = photoData
+                                self.tableView.reloadData()
+                                refreshControl.endRefreshing()
+                            }
+                        }
+                    }
+            })
+            task.resume()
+        }
+
+           }
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return photos.count
@@ -69,6 +107,8 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "PhotosTableViewCell") as! PhotosTableViewCell
+        
+//        let urlImages = photos[indexPath.row].value(forKeyPath: "images.standard_resolution.url") as! String
         let urlImages = photos[indexPath.row]["images"] as! NSDictionary
         let standarRes = urlImages["standard_resolution"] as! NSDictionary
         let urlString = standarRes["url"] as! String
